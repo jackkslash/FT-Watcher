74166e30c9f25fe298dbe34d0b456ddeeb9eee5a
mvotho <jackfaulknerbis@hotmail.co.uk>
2023-09-10 21:17:19 +0100

    added name check for notable names on new users

--- newuser.js
+++ newuser.js
@@ -1,25 +1,38 @@
 const axios = require('axios')
 require('dotenv').config()
+const notableNames = []
+
+function post(ws, m) {
+    axios.post(ws, m)
+        .then(response => {
+            console.log('Message sent successfully');
+        })
+        .catch(error => {
+            console.error('Error sending message');
+        });
+}
 
 async function check() {
-    let userindex = 0;
+    let userindex = 164434;
     while (true) {
         console.log(userindex)
         const req = await fetch("https://prod-api.kosetto.com/users/by-id/" + userindex)
         const res = await req.json();
 
         if (!res.message) {
-            message = {
-                content: 'https://www.friend.tech/rooms/' + res.address + "\n https://twitter.com/" + res.twitterUsername
-            };
+            let tw = res.twitterUsername;
+            if (notableNames.includes(tw)) {
+                message = {
+                    content: 'https://www.friend.tech/rooms/' + res.address + "\n https://twitter.com/" + res.twitterUsername + "\n @everyone notable name signed up",
+                    allowed_mentions: { "parse": ["everyone"] }
+                }
+            } else {
+                message = {
+                    content: 'https://www.friend.tech/rooms/' + res.address + "\n https://twitter.com/" + res.twitterUsername
+                };
+            }
 
-            axios.post(process.env.WBNU, message)
-                .then(response => {
-                    console.log('Message sent successfully');
-                })
-                .catch(error => {
-                    console.error('Error sending message');
-                });
+            post(process.env.WBNU, message)
         }
         while (res.message) {
             console.log("trying again")
@@ -28,17 +41,20 @@ async function check() {
             console.log(res)
             await new Promise(r => setTimeout(r, 2000));
             if (!res.message) {
-                message = {
-                    content: 'https://www.friend.tech/rooms/' + res.address + "\n https://twitter.com/" + res.twitterUsername
-                };
+                let tw = res.twitterUsername;
+                if (notableNames.includes(tw)) {
+                    message = {
+                        content: 'https://www.friend.tech/rooms/' + res.address + "\n https://twitter.com/" + res.twitterUsername + "\n @everyone notable name signed up",
+                        allowed_mentions: { "parse": ["everyone"] }
+                    }
+                } else {
+                    message = {
+                        content: 'https://www.friend.tech/rooms/' + res.address + "\n https://twitter.com/" + res.twitterUsername
+                    };
+                }
 
-                axios.post(process.env.WBNU, message)
-                    .then(response => {
-                        console.log('Message sent successfully');
-                    })
-                    .catch(error => {
-                        console.error('Error sending message');
-                    });
+
+                post(process.env.WBNU, message)
                 break;
             }
         }
@@ -48,5 +64,6 @@ async function check() {
         await new Promise(r => setTimeout(r, 1000));
     }
 }
+
 
 check();
